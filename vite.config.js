330ac4b2f525fdc871cf8da193b3381c3b99580e
Kumar Abhishek <manustays@users.noreply.github.com>
2026-07-09 17:21:15 +0530

    feat: service worker and PWA manifest for offline app shell

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	base: './',
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: false, // use public/manifest.webmanifest
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,woff2,png}'],
+				navigateFallback: 'index.html',
+			},
+		}),
+	],
 	test: {
 		environment: 'jsdom',
 		globals: true,
